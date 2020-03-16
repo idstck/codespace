d8e0e24293060f0de5f7107391fd3ff119322f23
idstack <idstack@outlook.co.id>
2020-03-16 09:03:05 +0000

    here maps integration with laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,11 @@
 
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-service.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-ui.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css" />
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js" type="text/javascript" charset="utf-8"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -82,5 +87,9 @@
 
     <script src="{{ asset('js/app.js') }}"></script>
     @stack('script')
+    <script>
+        window.hereApiKey = "{{ env('HERE_API_KEY') }}"
+    </script>
+    <script src="{{ asset('js/here.js') }}"></script>
 </body>
 </html>
