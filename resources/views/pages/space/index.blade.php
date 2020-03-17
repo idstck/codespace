197f43d795ffb40ba25c5a8366386bfcee2b02c9
idstack <idstack@outlook.co.id>
2020-03-17 06:12:41 +0000

    show codespace in list view

--- resources/views/pages/space/index.blade.php
+++ resources/views/pages/space/index.blade.php
@@ -5,20 +5,33 @@
     <x-space></x-space>
     <div class="row justify-content-center">
         <div class="col-md-8">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            @foreach ($spaces as $space)
             <div class="card">
-                <div class="card-header">Space</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    This is space list page.
+                    <h5 class="card-title">
+                        {{ $space->title }}
+                        <form action="#">
+                            @csrf @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger float-right">Delete</button>
+                            <a href="#" class="btn btn-sm btn-info float-right text-white">Edit</a>
+                        </form>
+                    </h5>
+                    <h6 class="card-subtitle">{{ $space->address }}</h6>
+                    <p class="card-text">{{ $space->description }}</p>
+                    <a href="#" class="card-link">Direction</a>
                 </div>
             </div>
+            @endforeach
         </div>
+    </div>
+    <div class="row justify-content-center">
+        {{ $spaces->links() }}
     </div>
 </div>
 @endsection
