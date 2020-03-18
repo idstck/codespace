e49415320d4ed643c078818188cddc89f54aa2bb
idstack <idstack@outlook.co.id>
2020-03-18 06:21:45 +0000

    show codespace in here map

--- app/Http/Controllers/SpaceController.php
+++ app/Http/Controllers/SpaceController.php
@@ -33,6 +33,11 @@ class SpaceController extends Controller
         return view('pages.space.create');
     }
 
+    public function browse()
+    {
+        return view('pages.space.browse');
+    }
+
     /**
      * Store a newly created resource in storage.
      *
